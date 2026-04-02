a7ebb5d9620adbf4e07dd171f9faf8dacb785cee
Mohsen Mahmoodi <m.mahmoodi.biz@gmail.com>
2026-04-03 00:18:59 +0300

    Remove memory storage

--- internal/container/container.go
+++ internal/container/container.go
@@ -52,9 +52,6 @@ func BuildContainer(rootCtx context.Context) (*dig.Container, error) {
 				}
 				return storage.NewPostgres(pool), nil
 
-			// case "memory":
-			// 	return storage.NewMemory(), nil
-
 			case "mysql":
 				// return storage.NewMySQL(cfg.DatabaseURL), nil (To be implemented)
 				return nil, fmt.Errorf("mysql storage not yet implemented")
