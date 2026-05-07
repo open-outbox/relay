32daa33f348011afe91e18a199ae5f45aebeb41e
Mohsen Mahmoodi <m.mahmoodi.biz@gmail.com>
2026-05-07 22:20:04 +0300

    fix: resolve config loadin issue for OTEL
    - The OTEL environment variables are not loaded by config,
    we need to load them from the env/.env in local development

--- cmd/relay/main.go
+++ cmd/relay/main.go
@@ -12,6 +12,7 @@ import (
 	"syscall"
 	"time"
 
+	"github.com/joho/godotenv"
 	"github.com/open-outbox/relay/internal/container"
 	"github.com/open-outbox/relay/internal/relay"
 	"github.com/open-outbox/relay/internal/telemetry"
@@ -21,6 +22,9 @@ import (
 
 // main initializes the application and starts the main execution loop.
 func main() {
+	// Needed for OTEL related environment variables
+	// that are not included in config.Config
+	_ = godotenv.Load()
 	if err := run(); err != nil {
 		log.Fatalf("Relay terminated with error: %v", err)
 	}
